10d39d57fdbdd5cdea99899a00673fc60bc342bf
Khairulhub <iubat21103033@gmail.com>
2022-09-29 10:09:39 +0600

    server make done

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT|| 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('wellcome to my server')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+console.log(`Doctors app listening on port ${port}`)
+})
