3e7b2c0e7f3973963fb8e8e0bae653a8dfbbdac7
Khairulhub <iubat21103033@gmail.com>
2022-09-29 12:07:37 +0600

    node mongo db and collection is set

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT|| 5000;
@@ -7,6 +8,26 @@ const port = process.env.PORT|| 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//mogodb connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zmhcerf.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+//doctors_portal
+
+//node mongodb api
+async function run(){
+   try{
+    await client.connect();
+    const servicesCollection=client.db("doctors_portal").collection("services");
+   }
+   finally{
+
+   }
+
+}
+run().catch(console.dir);
 
 
 
